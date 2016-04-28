19c47d1a396e31345569152ac1163e77b9eef6dd
houtaiw61@gmail.com <houtaiw61@gmail.com>
2016-04-27 23:10:03 -0400

    fixed the compMove by using recursion function

--- tic-tac-toe/ttt.js
+++ tic-tac-toe/ttt.js
@@ -1,8 +1,9 @@
 $(document).on('ready', function() {
   var winningCombos = [[1,2,3],[4,5,6],[7,8,9],[1,5,9],[3,5,7],[1,4,7],[2,5,8],[3,6,9]]
   var turn = 0;
-  var board = [1, 2, 3, 4, 5, 6, 7, 8 ,9]
-  attachTDhandler();
+  var board = [1, 2, 3, 4, 5, 6, 7, 8 ,9];
+  // attachTDhandler();
+  var player;
 
   function checkWinner(){
     for(var prop in winningCombos){
@@ -23,9 +24,9 @@ $(document).on('ready', function() {
 
   function checkTie(){
     if(turn === 9 && checkWinner() === false){
-      alert('tie game')
+      alert('tie game');
     }
-  }
+  };
 
   function attachTDhandler(){
     $('td').off('click');
@@ -50,36 +51,72 @@ $(document).on('ready', function() {
   function clearBoard(){
     $('td').html('').removeClass('o').removeClass('x');
     turn = 0;
-    attachTDhandler();
-  }
+    board = [1,2,3,4,5,6,7,8,9];
+    if(player === 0){
+      attachTDhandler();
+    }
+    else{
+      singleTDhandler();
+    }
+  };
 
   $('.reset').on('click', function(){
     clearBoard();
   });
 
-  // $('.double').on('click', function(){
-  //   attachTDhandler()
-  // })
-  //
-  // $('.single').on('click', function(){
-  //   if (turn % 2 === 0){
-  //     $(this).html('X').addClass('x');
-  //   }
-  //   else{
-  //     compMove();
-  //   }
-  //
-  //   //check for a winner
-  //   checkWinner();
-  //
-  //   // turns off the click listener for one of the table cells
-  //   $(this).off('click');
-  //   turn++;
-  //   checkTie();
-  // });
-  //
-  // function compMove(){
-  //
-  // }
+  $('.double').on('click', function(){
+    clearBoard();
+    player = 0;
+    attachTDhandler()
+  })
+
+  $('.single').on('click', function(){
+    clearBoard();
+    player = 1;
+    singleTDhandler()
+  });
+
+function singleTDhandler(){
+  $('td').off('click');
+  $('td').on('click', function() {
+
+    $(this).html('X').addClass('x');
+    $(compMove()).html('O').addClass('o').off('click');
+
+
+    $(this).off('click');
+    turn += 1.8;
+    //check for a winner
+    checkWinner();
+
+    // turns off the click listener for one of the table cells
+    checkTie();
+  });
+};
+
+  function compMove() {
+    // debugger
+    console.log('USER MOVE')
+
+    var ind = Math.floor(Math.random() * board.length)
+    var place = board[ind]
+
+    console.log(board)
+
+    if ($('.' + place).hasClass('x')){
+      console.log('USER fail x')
+      board.splice(ind, 1)
+      return compMove();
+    } else if ($('.' + place).hasClass('o')) {
+      console.log('USER fail o')
+      board.splice(ind, 1)
+      return compMove();
+    } else { //if (!($('#' + place).hasClass('x')) || !($('#' + place).hasClass('o'))) {
+      console.log('USER console')
+      board.splice(ind, 1)
+      return ('.' + place)
+      // debugger   <=== AWESOME SHIT USE ALWAYS
+    }
+  }
 
 });
